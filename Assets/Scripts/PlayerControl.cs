2b41d862bb13251a9be962de3a4c9bd00e8e5726
Darkchicken <joshshucker@hotmail.com>
2016-02-08 11:09:43 -0800

    More stuff added

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,21 +6,49 @@ public class PlayerControl : MonoBehaviour {
     public int health;
     //how far to move up and down to switch lanes
     private float laneWidth = 5;
+    private float shootForce = 5;
+
+    GameObject playerBullet;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        playerBullet = Resources.Load("PlayerBullet") as GameObject;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        Touch myTouch = Input.GetTouch(0);
-
-        Touch[] myTouches = Input.touches;
-        for (int i = 0; i < Input.touchCount; i++)
+        //check if there are touches
+        if (Input.touchCount > 0)
         {
-            //Do something with the touches
+            //if so, cycle through the touches
+            for (int i = 0; i < Input.touchCount; ++i)
+            {
+                Touch touch;
+                touch = Input.GetTouch(i);
+                if (touch.phase == TouchPhase.Began)
+                {
+                    GameObject bullet;
+                    bullet = (GameObject)Instantiate(playerBullet, transform.position, transform.rotation);
+
+                    // Add force to the cloned object in the object's forward direction
+                    bullet.GetComponent<Rigidbody>().AddForce(touch.position * shootForce);
+                }
+                if (touch.phase == TouchPhase.Moved)
+                {
+
+                }
+                else if (touch.phase == TouchPhase.Ended)
+                {
+
+                }
+            }
         }
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        
     }
 }
